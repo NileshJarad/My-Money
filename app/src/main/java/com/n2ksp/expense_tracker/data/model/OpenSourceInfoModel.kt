9c4b7c66f67e1ebc242d895ae9f8bff9dc1d208e
Nilesh Jarad <nlshjarad7@gmail.com>
2019-04-15 21:18:51 +0530

    UI font added for the OpenSource screen

--- app/src/main/java/com/n2ksp/expense_tracker/data/model/OpenSourceInfoModel.kt
+++ app/src/main/java/com/n2ksp/expense_tracker/data/model/OpenSourceInfoModel.kt
@@ -1,6 +1,12 @@
 package com.n2ksp.expense_tracker.data.model
 
-data class OpenSourceInfoModel(val libName: String, val copyright: String, val license: String, val website: String)
+data class OpenSourceInfoModel(
+    val libName: String,
+    val copyright: String,
+    val license: String,
+    val website: String,
+    var type: String = "(Lib)"
+)
 
 
 object OpenSourceInfoModelCreator {
@@ -33,6 +39,26 @@ object OpenSourceInfoModelCreator {
                 copyright = "Copyright 2013 Jake Wharton",
                 license = "Apache License, Version 2.0",
                 website = "https://github.com/JakeWharton/timber"
+            )
+        )
+
+        data.add(
+            OpenSourceInfoModel(
+                libName = "Muli",
+                copyright = "Copyright Google",
+                license = "",
+                website = "https://fonts.google.com/specimen/Muli?query=muli",
+                type = "(Font)"
+            )
+        )
+
+        data.add(
+            OpenSourceInfoModel(
+                libName = "Images ",
+                copyright = "Copyright Google",
+                license = "",
+                website = "https://material.io/tools/icons",
+                type = "(Asset Images)"
             )
         )
 
--- app/src/main/java/com/n2ksp/expense_tracker/ui/open_source/OpenSourceInfoAdapter.kt
+++ app/src/main/java/com/n2ksp/expense_tracker/ui/open_source/OpenSourceInfoAdapter.kt
@@ -30,17 +30,28 @@ class OpenSourceInfoAdapter @Inject constructor() : RecyclerView.Adapter<OpenSou
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.tvLibTitle.text = openSourceInfoList[position].libName
-        holder.tvLicense.text = openSourceInfoList[position].license
-        holder.tvCopyright.text = openSourceInfoList[position].copyright
-        holder.tvWebsite.text = openSourceInfoList[position].website
+        holder.libTitleTextView.text = openSourceInfoList[position].libName
+
+        holder.typeTextView.text = openSourceInfoList[position].type
+
+        val license = openSourceInfoList[position].license
+
+        if (license.isEmpty()) {
+            holder.licenseTextView.visibility = View.GONE
+        } else {
+            holder.licenseTextView.visibility = View.VISIBLE
+            holder.licenseTextView.text = license
+        }
+        holder.copyrightTextView.text = openSourceInfoList[position].copyright
+        holder.websiteTextView.text = openSourceInfoList[position].website
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var tvLibTitle: TextView = itemView.findViewById(R.id.tvLibTitle)
-        var tvCopyright: TextView = itemView.findViewById(R.id.tvCopyright)
-        var tvLicense: TextView = itemView.findViewById(R.id.tvLicense)
-        var tvWebsite: TextView = itemView.findViewById(R.id.tvWebsite)
+        var libTitleTextView: TextView = itemView.findViewById(R.id.libTitleTextView)
+        var typeTextView: TextView = itemView.findViewById(R.id.typeTextView)
+        var copyrightTextView: TextView = itemView.findViewById(R.id.copyrightTextView)
+        var licenseTextView: TextView = itemView.findViewById(R.id.licenseTextView)
+        var websiteTextView: TextView = itemView.findViewById(R.id.websiteTextView)
 
     }
 
